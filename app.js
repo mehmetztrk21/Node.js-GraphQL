0210f736aa91ee1b77cc615a831503346efda12e
Mehmet Öztürk <mehmet.ztrk2134@gmail.com>
2022-09-06 10:11:37 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const  { graphqlHTTP } = require('express-graphql')
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolver");
+
+const app = express();
+
+
+
+const { v4: uuidv4 } = require('uuid');
+
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4())
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use("/graphql", graphqlHTTP({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver
+}));
+
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    'mongodb://127.0.0.1:27017/chat'
+  )
+  .then(result => {
+    app.listen(8080);
+
+  })
+  .catch(err => console.log(err));
