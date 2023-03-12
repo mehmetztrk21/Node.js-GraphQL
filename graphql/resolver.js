6c8e0a0ff18c6b665805307f276581ea57ad6d24
mehmetztrk21 <mehmet.ztrk2134@gmail.com>
2023-03-12 14:09:30 +0300

    error handling

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
+import validator from "validator";
 const resolver = {
     hello() {
         return {
@@ -8,6 +9,20 @@ const resolver = {
         };
     },
     createUser: async function(args, req) {  //or {userInput}, req
+        const errors = [];
+        if(!validator.isEmail(args.userInput.email)) {
+            errors.push({message: "E-mail is invalid"});
+        }
+        if(validator.isEmpty(args.userInput.password) || !validator.isLength(args.userInput.password, {min: 5})) {
+            errors.push({message: "Password too short!"});
+        }
+        if(errors.length > 0) {
+            const error = new Error("Invalid input.");
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+
         const existingUser = await User.findOne({email: args.userInput.email});
         if(existingUser) {
             const error = new Error("User exists already!");
